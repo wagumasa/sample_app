e41568aa05370378599af2afc90994417aba445d
EC2 Default User <ec2-user@ip-172-31-19-201.us-east-2.compute.internal>
2019-08-30 13:40:49 +0000

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,13 +1,8 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
-  end
-
-  test "should get help" do
-    get static_pages_help_url
+  test "should get ..." do
+    get static_pages_..._url
     assert_response :success
   end
 
